e4336f097034d78fa3f23805b92d2dec9d99711e
Julienraptor01 <julienraptor01@gmail.com>
2023-11-23 09:19:30 +0100

    reformat

--- src/main/mainwindow.cpp
+++ src/main/mainwindow.cpp
@@ -4,63 +4,63 @@
 
 typedef struct
 {
-  int   id;
-  char  nom[40];
-  char  email[40];
+	int id;
+	char nom[40];
+	char email[40];
 } ELEMENT;
 
-ELEMENT Elm[] = 
-{ 
-  {0,"",""},
-  {1,"aaa","aaa@gmail.com"},
-  {2,"bbb","bbb@gmail.com"},
-  {3,"ccc","ccc@gmail.com"},
-  {4,"ddd","ddd@gmail.com"},
-  {5,"eee","eee@gmail.com"},
-  {0,"",""}
-};
+ELEMENT Elm[] =
+	{
+		{0, "", ""},
+		{1, "aaa", "aaa@gmail.com"},
+		{2, "bbb", "bbb@gmail.com"},
+		{3, "ccc", "ccc@gmail.com"},
+		{4, "ddd", "ddd@gmail.com"},
+		{5, "eee", "eee@gmail.com"},
+		{0, "", ""}
+	};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
-  ui->setupUi(this);
+	ui->setupUi(this);
 
-  //***** A modifier ***********************
-  setNom(Elm[1].nom);
-  setEmail(Elm[1].email);
-  //****************************************
+	//***** A modifier ***********************
+	setNom(Elm[1].nom);
+	setEmail(Elm[1].email);
+	//****************************************
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+	delete ui;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///// Fonctions utiles : ne pas modifier /////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
-void MainWindow::setNom(const char* Text)
+void MainWindow::setNom(const char *Text)
 {
-  fprintf(stderr,"---%s---\n",Text);
-  if (strlen(Text) == 0 )
-  {
-    ui->lineEditNom->clear();
-    return;
-  }
-  ui->lineEditNom->setText(Text);
+	fprintf(stderr, "---%s---\n", Text);
+	if (strlen(Text) == 0)
+	{
+		ui->lineEditNom->clear();
+		return;
+	}
+	ui->lineEditNom->setText(Text);
 }
 
-void MainWindow::setEmail(const char* Text)
+void MainWindow::setEmail(const char *Text)
 {
-  fprintf(stderr,"---%s---\n",Text);
-  if (strlen(Text) == 0 )
-  {
-    ui->lineEditEmail->clear();
-    return;
-  }
-  ui->lineEditEmail->setText(Text);
+	fprintf(stderr, "---%s---\n", Text);
+	if (strlen(Text) == 0)
+	{
+		ui->lineEditEmail->clear();
+		return;
+	}
+	ui->lineEditEmail->setText(Text);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -68,18 +68,18 @@ void MainWindow::setEmail(const char* Text)
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void MainWindow::on_pushButtonSuivant_clicked()
 {
-  fprintf(stderr,"Clic sur le bouton >>>\n");
-  // TO DO
+	fprintf(stderr, "Clic sur le bouton >>>\n");
+	// TO DO
 }
 
 void MainWindow::on_pushButtonPrecedent_clicked()
 {
-  fprintf(stderr,"Clic sur le bouton <<<\n");
-  // TO DO
+	fprintf(stderr, "Clic sur le bouton <<<\n");
+	// TO DO
 }
 
 void MainWindow::on_pushButtonQuitter_clicked()
 {
-  fprintf(stderr,"Clic sur le bouton Quitter\n");
-  // TO DO
+	fprintf(stderr, "Clic sur le bouton Quitter\n");
+	// TO DO
 }
--- src/main/mainwindow.h
+++ src/main/mainwindow.h
@@ -4,26 +4,29 @@
 #include <QMainWindow>
 
 QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
+namespace Ui
+{
+	class MainWindow;
+}
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
+	Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+	MainWindow(QWidget *parent = nullptr);
+	~MainWindow();
 
-    void setNom(const char* Text);
-    void setEmail(const char* Text);
+	void setNom(const char *Text);
+	void setEmail(const char *Text);
 
 private slots:
-    void on_pushButtonSuivant_clicked();
-    void on_pushButtonPrecedent_clicked();
-    void on_pushButtonQuitter_clicked();
+	void on_pushButtonSuivant_clicked();
+	void on_pushButtonPrecedent_clicked();
+	void on_pushButtonQuitter_clicked();
 
 private:
-    Ui::MainWindow *ui;
+	Ui::MainWindow *ui;
 };
 #endif // MAINWINDOW_H
